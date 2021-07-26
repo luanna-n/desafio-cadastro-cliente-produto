e7d65ee5cf208bdd8af258830ced768cd024670e
luanna-n <nayara.luanna@hotmail.com>
2021-07-26 10:55:43 -0300

    logica e layout adicionados

---
+++ script.js
@@ -0,0 +1,45 @@
+const mainForm = document.getElementById('main-form');
+
+mainForm.addEventListener('submit', function(e) {
+    e.preventDefault();
+
+    // Adicionando o produto na tabela
+
+    let codigo = document.getElementById('codigo').value;
+    let nome = document.getElementById('name').value;
+    let marcaCPF = document.getElementById('marca-cpf').value;
+    let qtdeTEL = document.getElementById('qtde-tel').value;
+    let obsEND = document.getElementById('obs-end').value;
+
+    let dadosForm = {
+        codigo,
+        nome,
+        marcaCPF,
+        qtdeTEL,
+        obsEND
+    };
+
+    let valoresDadosForm = Object.values(dadosForm);
+
+    let tbody = document.getElementById('tabela-items');
+    let tr = document.createElement('tr');
+    tbody.append(tr);
+
+    valoresDadosForm.forEach(valor => {
+        let td = document.createElement('td');
+        tr.append(td);
+        td.append(valor);
+    });
+
+    // Adicionando o produto no Local Storage
+
+    let info = JSON.parse(localStorage.getItem('informacao')) ?? [];
+
+    info.push(dadosForm);
+
+    let infoConvertida = JSON.stringify(info);
+
+    localStorage.setItem('informacao', infoConvertida);
+
+    swal('Tudo certo!', 'Produto cadastrado com sucesso!', 'success');
+});
